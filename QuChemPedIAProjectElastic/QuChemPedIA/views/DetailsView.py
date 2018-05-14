e07ebff935b783aebf597ea6d1254783efdbc23b
bharismendy <briceharismendy@gmail.com>
2018-05-14 15:13:16 +0200

    remplacement du rest par un variable django

--- QuChemPedIAProjectElastic/QuChemPedIA/views/DetailsView.py
+++ QuChemPedIAProjectElastic/QuChemPedIA/views/DetailsView.py
@@ -1,14 +1,10 @@
 from django.shortcuts import render
 from QuChemPedIA.search import *
-from rest_framework.decorators import api_view, renderer_classes
-from rest_framework.response import Response
-from rest_framework.renderers import TemplateHTMLRenderer, JSONRenderer
 
 
-@api_view(['GET', ])
-@renderer_classes((JSONRenderer, TemplateHTMLRenderer))
 def details(request, id):
     # function that show details of molecule
-    result = search_id(id)
-    return Response(data=result, template_name='QuChemPedIA/details.html')
+    results = search_id(id)
+    return render(request, 'QuChemPedIA/details.html', {'results': results})
+
 
