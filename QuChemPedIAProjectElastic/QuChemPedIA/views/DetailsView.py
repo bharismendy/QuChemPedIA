5e578ec40217a8e48c7bfad4891724c528ad8f09
bharismendy <briceharismendy@gmail.com>
2018-05-15 15:15:44 +0200

    bug fix detail

--- QuChemPedIAProjectElastic/QuChemPedIA/views/DetailsView.py
+++ QuChemPedIAProjectElastic/QuChemPedIA/views/DetailsView.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from QuChemPedIA.forms.QueryForm import QueryForm
 from QuChemPedIA.search import *
+from django.http.response import HttpResponseRedirect
 
 
 def details(request, id):
@@ -10,5 +11,8 @@ def details(request, id):
     :param id: id of the json that we want to show
     :return: html template
     """
+    query_form = QueryForm(request.GET or None)
+    if query_form.is_valid():
+        return HttpResponseRedirect('query')
     results = search_id(id)
-    return render(request, 'QuChemPedIA/details.html', {'results': results, 'cid': id,})
+    return render(request, 'QuChemPedIA/details.html', {'results': results, 'cid': id, 'query_form': query_form})
