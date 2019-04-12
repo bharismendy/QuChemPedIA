20ff34d8e7e02a261e8907b4d4435b8680ea56b1
bharismendy <brice.harismendy@etud.univ-angers.fr>
2019-04-12 15:30:25 +0200

    documentation et automatisation des tests

--- QuChemPedIAProjectElastic/install_venv.sh
+++ QuChemPedIAProjectElastic/install_venv.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 echo "suppression du dossier venv..."
-#rm -rf venv
+rm -rf venv
 echo "création de l'environement virtuel..."
-#virtualenv venv -p python3.6
+virtualenv venv -p python3.6
 echo "installation des package python..."
 source venv/bin/activate
-venv/bin/pip3.6 --proxy http://trendupdate:opla5271@proxy.cg49.fr:8080 install -r requirements.txt 
+venv/bin/pip3.6 install -r requirements.txt 
 deactivate
 echo "fin de l'installation de l'environement virtuel ..."
