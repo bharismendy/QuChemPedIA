949b4c05966ef9cccb7e8abe960df9b701a8b863
Paulin Violette <violette.paulin@gmail.com>
2019-05-24 10:13:37 +0200

    Added button to show jsmol viz

--- QuChemPedIAProject/assets/js/details/components/QcpiaMoleculeVizJsMol.vue
+++ QuChemPedIAProject/assets/js/details/components/QcpiaMoleculeVizJsMol.vue
@@ -3,7 +3,14 @@
     ref="container"
     class="w-100"
   >
+    <b-btn
+      variant="primary"
+      @click="toggleViz"
+    >
+      Toggle visualisation
+    </b-btn>
     <iframe
+      v-if="displayJsmol"
       :src="vizUrl"
       class="w-100 border-0"
       height="500"
@@ -20,9 +27,19 @@ export default {
       required: true
     }
   },
+  data () {
+    return {
+      displayJsmol: false
+    }
+  },
   computed: {
     vizUrl () {
       return `${this.$root.baseUrl}/access/viz?log_file=${this.metadata.log_file}`
+    }
+  },
+  methods: {
+    toggleViz () {
+      this.displayJsmol = !this.displayJsmol
     }
   }
 }
